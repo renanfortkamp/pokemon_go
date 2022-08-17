5d7f7dc3f6c8028243d70b7223690c5bd0d110f2
renanfort <renan_fortkamp@estudante.sesisenai.org.br>
2022-08-17 00:19:45 -0300

    função login usuario

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -10,23 +10,46 @@ import {
 } from "react-native";
 
 import { useState } from "react";
-import background from "../../../assets/bg.jpg"
-export default function Login({navigation}) {
+import background from "../../../assets/bg.jpg";
+import NewPlayer from "../NewPlayer/NewPlayer";
+import { players } from "../NewPlayer/NewPlayer";
 
-    function navigationHome(){
-        setTextoVisto(texto)
-        navigation.navigate('Home')
-    }
-
-    function navigationNewPlayer(){
-        
-        navigation.navigate('NewPlayer')
-    }
-
-    const [texto, setTexto] = useState("");
-    const [senha, setSenha] = useState("");
+export default function Login({ navigation }) {
+    const players1 = [
+        { email: "renan", senha: "renan" },
+        { email: "renan1", senha: "renan1" },
+        { email: "renan2", senha: "renan2" },
+        { email: "renan3", senha: "renan3" },
+        { email: "renan4", senha: "renan4" },
+        { email: "renan5", senha: "renan5" },
+        { email: "renan6", senha: "renan6" },
+        { email: "renan7", senha: "renan7" },
+        { email: "renan8", senha: "renan8" },
+        { email: "renan9", senha: "renan9" },
+    ];
+    const [emailLogin, setEmailLogin] = useState("");
+    const [senhaLogin, setSenhaLogin] = useState("");
 
     const [textoVisto, setTextoVisto] = useState("");
+    function userLogin() {
+        if(players.length > 0){
+            for (let i = 0; i < players1.length; i++) {
+                if (
+                    players[i].email == emailLogin &&
+                    players[i].senha == senhaLogin
+                ){
+                    alert("Usuario Encontrado");
+                    return;
+                }
+            }
+            
+        }else{alert("Usuario não cadastrado")}
+        alert("não encontrado")
+    }
+
+    function navigationNewPlayer() {
+        navigation.navigate("NewPlayer");
+    }
 
     return (
         <ImageBackground
@@ -38,39 +61,32 @@ export default function Login({navigation}) {
 
             <TextInput
                 style={style.input}
-                onChangeText={(text) => setTexto(text)}
+                onChangeText={(emailLogin) => setEmailLogin(emailLogin)}
                 placeholder="Digite seu e-mail"
                 backgroundColor="#fff"
                 placeholderTextColor={"red"}
                 textAlign={"center"}
             />
             <TextInput
-                secureTextEntry={true}
+                // secureTextEntry={true}
                 style={style.input}
-                onChangeText={(senha) => setSenha(senha)}
+                onChangeText={(senhaLogin) => setSenhaLogin(senhaLogin)}
                 placeholder="Digite sua senha"
                 placeholderTextColor={"red"}
                 textAlign={"center"}
                 backgroundColor="#fff"
             />
 
-            <TouchableOpacity
-                onPress={navigationHome}
-                style={style.button}
-            >
-                <Text style={{ fontSize: 20, color: '#fff' }}>
-                    Entrar
-                </Text>
-                
+            <TouchableOpacity onPress={userLogin} style={style.button}>
+                <Text style={{ fontSize: 20, color: "#fff" }}>Entrar</Text>
             </TouchableOpacity>
-            <TouchableOpacity onPress={navigationNewPlayer} >
-            <Text style={{ fontSize: 15, color: '#fff' }}>
+            <TouchableOpacity onPress={navigationNewPlayer}>
+                <Text style={{ fontSize: 15, color: "#fff" }}>
                     Ainda não tem cadastro ?
                 </Text>
             </TouchableOpacity>
 
             <Text>{textoVisto}</Text>
-            
         </ImageBackground>
     );
 }
@@ -90,10 +106,10 @@ const style = StyleSheet.create({
     button: {
         flexDirection: "row",
         justifyContent: "center",
-        alignItems:'center',
+        alignItems: "center",
         width: 120,
         height: 50,
-        borderColor: '#DC5F43',
+        borderColor: "#DC5F43",
         borderWidth: 2,
         backgroundColor: "#0D4DA3",
         marginBottom: 10,
@@ -106,14 +122,14 @@ const style = StyleSheet.create({
         height: 50,
     },
     input: {
-      backgroundColor: '#FFF',
-      height: 50,
-      width: '70%',
-      borderColor: '#DC5F43',
-      borderWidth: 3,
-      borderRadius: 10,
-      textAlign: 'center',
-      color: '#DC5F43',
-      marginBottom:10,
+        backgroundColor: "#FFF",
+        height: 50,
+        width: "70%",
+        borderColor: "#DC5F43",
+        borderWidth: 3,
+        borderRadius: 10,
+        textAlign: "center",
+        color: "#DC5F43",
+        marginBottom: 10,
     },
 });
--- src/pages/NewPlayer/NewPlayer.js
+++ src/pages/NewPlayer/NewPlayer.js
@@ -1,13 +1,44 @@
-import { StyleSheet, Text, View, TextInput, ImageBackground, TouchableOpacity } from 'react-native'
-import React from 'react'
-import background from "../../../assets/bg.jpg"
+import {
+    StyleSheet,
+    Text,
+    View,
+    TextInput,
+    ImageBackground,
+    TouchableOpacity,
+} from "react-native";
+import React from "react";
+import { useState } from "react";
 
-export default function NewPlayer() {
-  return (
-    <View style={style.container}>
-      <TextInput
+export const players = [];
+
+export default function NewPlayer({ navigation }) {
+    const [nick, setNick] = useState("");
+    const [idade, setIdade] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [email, setEmail] = useState("");
+    const [senha, setSenha] = useState("");
+
+    function cadastrarPlayer() {
+        if (nick && idade && telefone && email && senha != "") {
+            players.push({
+                nick: nick,
+                idade: idade,
+                telefone: telefone,
+                email: email,
+                senha: senha,
+            });
+
+            navigation.navigate("Login");
+        } else {
+            alert("Preencha todos os dados!");
+        }
+    }
+
+    return (
+        <View style={style.container}>
+            <TextInput
                 style={style.input}
-                // onChangeText={(name) => setTexto(name)}
+                onChangeText={(nick) => setNick(nick)}
                 placeholder="Digite seu nickname"
                 backgroundColor="#fff"
                 placeholderTextColor={"#0d4da4"}
@@ -16,21 +47,21 @@ export default function NewPlayer() {
             />
             <TextInput
                 style={style.input}
-                // onChangeText={(idade) => setSenha(idade)}
+                onChangeText={(idade) => setIdade(idade)}
                 placeholder="Digite sua idade"
                 placeholderTextColor={"#0d4da4"}
                 selectionColor={"#0d4da4"}
                 textAlign={"center"}
-                keyboardType={'number-pad'}
+                keyboardType={"number-pad"}
                 maxLength={3}
                 backgroundColor="#fff"
             />
             <TextInput
                 style={style.input}
-                // onChangeText={(name) => setTexto(name)}
+                onChangeText={(telefone) => setTelefone(telefone)}
                 placeholder="Digite seu telefone Ex:(DDD)NUMERO"
                 backgroundColor="#fff"
-                keyboardType={'number-pad'}
+                keyboardType={"number-pad"}
                 maxLength={12}
                 placeholderTextColor={"#0d4da4"}
                 selectionColor={"#0d4da4"}
@@ -38,77 +69,77 @@ export default function NewPlayer() {
             />
             <TextInput
                 style={style.input}
-                // onChangeText={(name) => setTexto(name)}
+                onChangeText={(email) => setEmail(email)}
                 placeholder="Digite seu e-mail"
                 backgroundColor="#fff"
                 placeholderTextColor={"#0d4da4"}
                 selectionColor={"#0d4da4"}
                 textAlign={"center"}
             />
-                <TextInput
+            <TextInput
                 secureTextEntry={true}
                 style={style.input}
-                // onChangeText={(idade) => setSenha(idade)}
+                onChangeText={(senha) => setSenha(senha)}
                 placeholder="Digite sua senha"
                 placeholderTextColor={"#0d4da4"}
                 selectionColor={"#0d4da4"}
                 textAlign={"center"}
-                keyboardType={'number-pad'}
+                keyboardType={"number-pad"}
                 maxLength={8}
                 backgroundColor="#fff"
             />
 
-<TouchableOpacity
-            style={style.button}
-            >
-                <Text style={{ fontSize: 25, color: '#fff' }}>
-                    Cadastrar
-                </Text>
-                
+            <TouchableOpacity style={style.button} onPress={cadastrarPlayer}>
+                <Text style={{ fontSize: 25, color: "#fff" }}>Cadastrar</Text>
             </TouchableOpacity>
-    </View>
-  )
+            <Text>{nick}</Text>
+            <Text>{idade}</Text>
+            <Text>{telefone}</Text>
+            <Text>{email}</Text>
+            <Text>{senha}</Text>
+        </View>
+    );
 }
 
 const style = StyleSheet.create({
-  background: {
-      flex: 1,
-      justifyContent: "center",
-      alignItems: "center",
-  },
-  container: {
-      paddingTop:10,
-      flex: 1,
-      backgroundColor: "#fff",
-      alignItems: "center",
-  },
-  button: {
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems:'center',
-      width: 200,
-      height: 60,
-      borderColor: '#fff',
-      borderWidth: 2,
-      backgroundColor: "#0D4DA3",
-      marginBottom: 10,
-      borderRadius: 10,
-  },
-  img: {
-      marginLeft: 5,
-      marginRight: 20,
-      width: 50,
-      height: 50,
-  },
-  input: {
-    backgroundColor: '#FFF',
-    height: 50,
-    width: '70%',
-    borderColor: '#0D4DA3',
-    borderWidth: 3,
-    borderRadius: 5,
-    textAlign: 'center',
-    color: '#0D4DA3',
-    marginBottom:10,
-  },
+    background: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    container: {
+        paddingTop: 10,
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+    },
+    button: {
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+        width: 200,
+        height: 60,
+        borderColor: "#fff",
+        borderWidth: 2,
+        backgroundColor: "#0D4DA3",
+        marginBottom: 10,
+        borderRadius: 10,
+    },
+    img: {
+        marginLeft: 5,
+        marginRight: 20,
+        width: 50,
+        height: 50,
+    },
+    input: {
+        backgroundColor: "#FFF",
+        height: 50,
+        width: "70%",
+        borderColor: "#0D4DA3",
+        borderWidth: 3,
+        borderRadius: 5,
+        textAlign: "center",
+        color: "#0D4DA3",
+        marginBottom: 10,
+    },
 });
